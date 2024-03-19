2f6e067e7678fc8e9290794ee6d68e1b5b975edf
Adam Boucek <adam@boucek.dev>
2024-03-19 14:58:47 -0700

    feat: add ui library

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,6 +6,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 
 import { useColorScheme } from '@/components/useColorScheme';
+import { GluestackUIProvider } from '@gluestack-ui/themed';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -48,11 +49,14 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
+    <GluestackUIProvider>
+
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
       </Stack>
     </ThemeProvider>
+    </GluestackUIProvider>
   );
 }
