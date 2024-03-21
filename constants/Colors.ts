4c3d8cda0b98f128f0aa8c4929c6a23ee8f6f513
Adam Boucek <adam@boucek.dev>
2024-03-20 19:15:17 -0700

    style: add global styles

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -16,4 +16,13 @@ export default {
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorDark,
   },
+  blue: '#428fec',
+  primary: '#CC8C54',
+  secondary: '#81B15E',
+  accent1: '#FFD9B6',
+  accent2: '#C2DFAD',
+  background1: '#E6F5DB',
+  background2: '#FFEFE0',
+  black: '#212121',
+  white: '#FFFCF6',
 };
